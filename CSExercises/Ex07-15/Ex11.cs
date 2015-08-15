df6ccd22486b33faaa9b3f151349dfe1b47c23d5
AdeleTan <e0005472@u.nus.edu>
2015-08-15 15:46:32 +0800

    Ex13 Completed

--- CSExercises/Ex07-15/Ex11.cs
+++ CSExercises/Ex07-15/Ex11.cs
@@ -10,6 +10,16 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            System.Console.WriteLine ("Enter the distance travelled");
+            double fare = Convert.ToDouble(System.Console.ReadLine());
+
+            double fare1 = 2.4 + (0.4 * fare);
+
+            System.Console.WriteLine("The fare for the distance travelled is {0}", fare1);
+
+
+
+
             
         }
     }
--- CSExercises/Ex07-15/Ex13.cs
+++ CSExercises/Ex07-15/Ex13.cs
@@ -10,7 +10,13 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            System.Console.WriteLine("Enter the distance travelled");
+            double fare = Convert.ToDouble(System.Console.ReadLine());
+
+            double fare1 = 2.4 + (0.4 * fare);
+
+            System.Console.WriteLine("The fare for the distance travelled is {0:#.0}", fare1);
+ 
         }
     }
 }
